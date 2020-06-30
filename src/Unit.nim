6c10b9794760d0db275c3ecb7475e70912f57080
Momeemt <momiimt@gmail.com>
2020-06-30 21:18:24 +0900

    ✨ 型定義・モジュールの呼び出しを実装

--- src/Unit.nim
+++ src/Unit.nim
@@ -1,2 +1,48 @@
-import Unit/[types, gram, kilogram, milligram]
-export types, gram, kilogram, milligram
+import Unit/Types
+export Types
+  
+import Unit/Gram
+export Gram
+  
+import Unit/KiloGram
+export KiloGram
+  
+import Unit/MilliGram
+export MilliGram
+  
+import Unit/Metre
+export Metre
+  
+import Unit/KiloMetre
+export KiloMetre
+  
+import Unit/CentiMetre
+export CentiMetre
+  
+import Unit/MilliMetre
+export MilliMetre
+  
+import Unit/Second
+export Second
+  
+import Unit/MilliSecond
+export MilliSecond
+  
+import Unit/Ampere
+export Ampere
+  
+import Unit/MilliAmpere
+export MilliAmpere
+  
+import Unit/KiloAmpere
+export KiloAmpere
+  
+import Unit/Kelvin
+export Kelvin
+  
+import Unit/Mole
+export Mole
+  
+import Unit/Candela
+export Candela
+  
