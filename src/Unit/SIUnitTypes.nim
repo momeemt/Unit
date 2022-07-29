45857489e780e2ba7f7f5ecf9074561f1c840173
momeemt <momiimt@gmail.com>
2022-07-29 17:51:02 +0900

    👍 add: SIUnitTypes

--- src/Unit/SIUnitTypes.nim
+++ src/Unit/SIUnitTypes.nim
@@ -1,45 +1,19 @@
-import typeDefinition
+import core
 
-const
-  kg* = block:
-    var u: Unit
-    u[siKg] = 1
-    u
-  ## The type kg represents the mass in the International System of Units (SI) in kilograms.
-  
-  m* = block:
-    var u: Unit
-    u[siM] = 1
-    u
-  ## The type m represents the length in the International System of Units (SI) in metres.
-  
-  s* = block:
-    var u: Unit
-    u[siS] = 1
-    u
-  ## The type s represents the time in the International System of Units (SI) in seconds.
-  
-  A* = block:
-    var u: Unit
-    u[siA] = 1
-    u
-  ## The type A represents the electric current in the International System of Units (SI) in amperes.
-  
-  K* = block:
-    var u: Unit
-    u[siK] = 1
-    u
-  ## The type K represents the temperature in the International System of Units (SI) in kelvins.
-  
-  mol* = block:
-    var u: Unit
-    u[siMol] = 1
-    u
-  ## The type mol represents the amount of substance in the International System of Units (SI) in moles.
-  
-  cdl* = block:
-    var u: Unit
-    u[siCd] = 1
-    u
-  ## The type cdl represents the luminous intensity in the International System of Units (SI) in candelas.
-  ## [Notice!!] Since the Nim standard library provides cd procedures, the type name is set to cdl.
+proc kg* [T: SomeNumber] (value: T): Unit[ud(kg=1), T] =
+  result = Unit[ud(kg=1), T](value: value)
+
+proc s* [T: SomeNumber] (value: T): Unit[ud(s=1), T] =
+  result = Unit[ud(s=1), T](value: value)
+
+proc A* [T: SomeNumber] (value: T): Unit[ud(A=1), T] =
+  result = Unit[ud(A=1), T](value: value)
+
+proc K* [T: SomeNumber] (value: T): Unit[ud(K=1), T] =
+  result = Unit[ud(K=1), T](value: value)
+
+proc mol* [T: SomeNumber] (value: T): Unit[ud(mol=1), T] =
+  result = Unit[ud(mol=1), T](value: value)
+
+proc cd* [T: SomeNumber] (value: T): Unit[ud(cd=1), T] =
+  result = Unit[ud(cd=1), T](value: value)
